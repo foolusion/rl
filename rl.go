fd5d5c254a8c3bd1a13501ccea49e62bba37a0c6
Andrew O'Neill <foolusion@gmail.com>
2016-12-24 20:04:28 +0000

    step 2

--- rl.go
+++ rl.go
@@ -13,10 +13,89 @@ func main() {
 	}
 	defer termbox.Close()
 
-	termbox.SetCell(0, 0, '@', termbox.ColorDefault, termbox.ColorDefault)
+	var g game
+
+	termbox.SetCell(g.playerx, g.playery, '@', termbox.ColorDefault, termbox.ColorDefault)
 	if err := termbox.Flush(); err != nil {
 		log.Fatal(err)
 	}
 
-	termbox.PollEvent()
+	for !g.done {
+		// get input
+		var action string
+		ev := termbox.PollEvent()
+		switch ev.Type {
+		case termbox.EventKey:
+			action = handleEvent(&g, ev)
+		case termbox.EventError:
+			break
+		}
+
+		// update
+		switch action {
+		case "MOVE_UP":
+			g.playery -= 1
+		case "MOVE_DOWN":
+			g.playery += 1
+		case "MOVE_RIGHT":
+			g.playerx += 1
+		case "MOVE_LEFT":
+			g.playerx -= 1
+		case "QUIT":
+			g.done = true
+		default:
+			continue
+		}
+
+		// render
+		if err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault); err != nil {
+			log.Fatal(err)
+		}
+		termbox.SetCell(g.playerx, g.playery, '@', termbox.ColorDefault, termbox.ColorDefault)
+		if err := termbox.Flush(); err != nil {
+			log.Fatal(err)
+		}
+
+	}
+}
+
+type game struct {
+	playerx int
+	playery int
+	done    bool
+}
+
+func handleEvent(g *game, ev termbox.Event) string {
+	switch ev.Ch {
+	case 0:
+		return handleKey(g, ev.Key)
+	case 'h', 'H':
+		return "MOVE_LEFT"
+	case 'j', 'J':
+		return "MOVE_DOWN"
+	case 'k', 'K':
+		return "MOVE_UP"
+	case 'l', 'L':
+		return "MOVE_RIGHT"
+	default:
+		return ""
+	}
+
+}
+
+func handleKey(g *game, k termbox.Key) string {
+	switch k {
+	case termbox.KeyArrowUp:
+		return "MOVE_UP"
+	case termbox.KeyArrowDown:
+		return "MOVE_DOWN"
+	case termbox.KeyArrowRight:
+		return "MOVE_RIGHT"
+	case termbox.KeyArrowLeft:
+		return "MOVE_LEFT"
+	case termbox.KeyEsc:
+		return "QUIT"
+	default:
+		return ""
+	}
 }
