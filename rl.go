2910cda58dc22d1463b88d8dbf1217d41c808595
Andrew O'Neill <foolusion@gmail.com>
2016-12-24 19:07:56 +0000

    initial commit python tutorial

--- rl.go
+++ rl.go
@@ -1,62 +1,22 @@
-package rl
+package main
 
-import "time"
+import (
+	"log"
 
-const player = 0
+	termbox "github.com/nsf/termbox-go"
+)
 
-// Game holds all the state for a game.
-type game struct {
-	positions        map[int]V
-	actors           map[int]string
-	inputController  InputController
-	renderController RenderController
-	turn             int
-	gMap             Map
-	done             bool
-	err              error
-}
-
-// NewGame creates a new game using the controller supplied.
-func NewGame(c Controller) *game {
-	return &game{
-		positions: map[int]V{0: V{1, 1, 0, 0}},
-		actors:    map[int]string{0: "player"},
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		log.Fatal(err)
 	}
-}
+	defer termbox.Close()
 
-func (g *game) Run() error {
-	var err error
-	lastStep := time.Now()
-	for err != nil {
-		err = g.Step()
-		if time.Since(lastStep) > 15*time.Millisecond {
-			time.Sleep(15*time.Millisecond - time.Since(lastStep))
-		}
-		lastStep = time.Now()
+	termbox.SetCell(0, 0, '@', termbox.ColorDefault, termbox.ColorDefault)
+	if err := termbox.Flush(); err != nil {
+		log.Fatal(err)
 	}
-	return err
-}
 
-func (g *game) Step() error {
-	g.inputController.GetInput(g)
-	update(g)
-	g.renderController.Render(g)
-	if g.err != nil {
-		return g.err
-	}
-	return nil
-}
-
-func update(g *game) {
-}
-
-// Player holds all the state for the player.
-type Player struct {
-	Position   V
-	Controller Controller
-}
-
-// Controller is an interface for getting actions.
-type Controller interface {
-	Get() Input
+	termbox.PollEvent()
 }
